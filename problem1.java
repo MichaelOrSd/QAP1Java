2d4836fbd1c2fae456c117ff2f199852163aab75
michaelorsd <michael.oreilly@keyin.com>
2022-05-16 20:08:21 -0230

    changes

---
+++ problem1.java
@@ -0,0 +1,13 @@
+// comment 
+
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("The number is " + num);
+    }
+}
+
